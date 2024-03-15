a6444e3fdaaad63b253ee0f381d8611de3ed1a51
NANDA BHARATH <nandamh28@gmail.com>
2024-03-15 22:40:32 +0530

    selenium first class

---
+++ src/test/java/org/Testingacademy/Testcase01.java
@@ -0,0 +1,7 @@
+package org.Testingacademy;
+
+public class Testcase01 {
+    public static void main(String[] args) {
+        System.out.println("nanda");
+    }
+}
---
+++ src/test/java/org/Testingacademy/Testcase03.java
@@ -0,0 +1,16 @@
+package org.Testingacademy;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcase03 {
+    @Test
+    public void testvwoLogin(){
+        // system.property to setup the path of the Drivers
+        // Chrome -> ChromeDriver
+        // Edge -> EdgeDriver
+        // Firefox -> GeckoDriver
+        // Safari -> Safaridriver
+        Assert.assertTrue(false);
+    }
+}
